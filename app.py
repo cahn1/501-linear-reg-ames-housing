a4afdd4f2de985d79fccda9e2247d69e515d4075
Kayden Ahn <kayden_ahn@intuit.com>
2022-04-10 23:35:30 -0700

    update format

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 import dash
-from dash import dcc,html
+from dash import dcc, html
 from dash.dependencies import Input, Output, State
 
 
-
-########### Define your variables ######
-myheading1='Predicting Home Sale Prices in Ames, Iowa'
-image1='ames_welcome.jpeg'
+# Global variables
+myheading1 = 'Predicting Home Sale Prices in Ames, Iowa'
+image1 = 'ames_welcome.jpeg'
 tabtitle = 'Ames Housing'
 sourceurl = 'http://jse.amstat.org/v19n3/decock.pdf'
 githublink = 'https://github.com/plotly-dash-apps/501-linear-reg-ames-housing'
 
 
-########### Initiate the app
+# app server config
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title = tabtitle
 
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading1),
-    html.Div([
-        html.Img(src=app.get_asset_url(image1), style={'width': '30%', 'height': 'auto'}, className='four columns'),
+# app component layout
+app.layout = html.Div(
+    children=[
+        html.H1(myheading1),
         html.Div([
+            html.Img(src=app.get_asset_url(image1), style={'width': '30%', 'height': 'auto'}, className='four columns'),
+            html.Div([
                 html.H3('Features of Home:'),
                 html.Div('Year Built:'),
                 dcc.Input(id='YearBuilt', value=2010, type='number', min=2006, max=2010, step=1),
@@ -37,38 +37,37 @@ app.layout = html.Div(children=[
                 dcc.Input(id='SingleFam', value=0, type='number', min=0, max=1, step=1),
                 html.Div('Large Neighborhood:'),
                 dcc.Input(id='LargeNeighborhood', value=0, type='number', min=0, max=1, step=1),
-
-            ], className='four columns'),
+                ], className='four columns'),
             html.Div([
-                html.Button(children='Submit', id='submit-val', n_clicks=0,
-                                style={
-                                'background-color': 'red',
-                                'color': 'white',
-                                'margin-left': '5px',
-                                'verticalAlign': 'center',
-                                'horizontalAlign': 'center'}
-                                ),
+                html.Button(
+                    children='Submit', id='submit-val', n_clicks=0,
+                    style={
+                        'background-color': 'red',
+                        'color': 'white',
+                        'margin-left': '5px',
+                        'verticalAlign': 'center',
+                        'horizontalAlign': 'center'}
+                ),
                 html.H3('Predicted Home Value:'),
                 html.Div(id='Results')
             ], className='four columns')
-        ], className='twelve columns',
-    ),
-    html.Br(),
-    html.Br(),
-    html.Br(),
-    html.H4('Regression Equation:'),
-    html.Div('Predicted Price = (- $1,360.5K Baseline) + ($0.7K * Year Built) + ($12.7K * Bathrooms) + (- $7.7K * Bedrooms) + ($0.049K * Total Square Feet) + ($ 25.2K * Single Family Home) + (- $6.6 K * Large Neighborhood)'),
-    html.Br(),
-    html.A('Google Spreadsheet', href='https://docs.google.com/spreadsheets/d/1q2ustRvY-GcmPO5NYudvsBEGNs5Na5p_8LMeS4oM35U/edit?usp=sharing'),
-    html.Br(),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
+        ], className='twelve columns',),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.H4('Regression Equation:'),
+        html.Div('Predicted Price = (- $1,360.5K Baseline) + ($0.7K * Year Built) + ($12.7K * Bathrooms) + (- $7.7K * Bedrooms) + ($0.049K * Total Square Feet) + ($ 25.2K * Single Family Home) + (- $6.6 K * Large Neighborhood)'),
+        html.Br(),
+        html.A('Google Spreadsheet', href='https://docs.google.com/spreadsheets/d/1q2ustRvY-GcmPO5NYudvsBEGNs5Na5p_8LMeS4oM35U/edit?usp=sharing'),
+        html.Br(),
+        html.A('Code on Github', href=githublink),
+        html.Br(),
+        html.A("Data Source", href=sourceurl),
     ]
 )
 
 
-######### Define Callback
+# callback
 @app.callback(
     Output(component_id='Results', component_property='children'),
     Input(component_id='submit-val', component_property='n_clicks'),
@@ -80,7 +79,8 @@ app.layout = html.Div(children=[
     State(component_id='LargeNeighborhood', component_property='value')
 
 )
-def ames_lr_function(clicks, YearBuilt,Bathrooms,BedroomAbvGr,TotalSF,SingleFam,LargeNeighborhood):
+def ames_lr_function(clicks, YearBuilt, Bathrooms, BedroomAbvGr, TotalSF,
+                     SingleFam,LargeNeighborhood):
     if clicks==0:
         return "waiting for inputs"
     else:
@@ -89,7 +89,6 @@ def ames_lr_function(clicks, YearBuilt,Bathrooms,BedroomAbvGr,TotalSF,SingleFam,
         return formatted_y
 
 
-
-############ Deploy
+# Initiate app
 if __name__ == '__main__':
     app.run_server(debug=True)
