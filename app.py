b00d84dc209915ecab74367d7d5c1f8e620c361b
Kayden Ahn <kayden_ahn@intuit.com>
2022-04-12 01:08:55 -0700

    updated using page tab

--- app.py
+++ app.py
@@ -24,102 +24,102 @@ app.layout = html.Div([
     html.H1('Linear regression analysis', style={
         'textAlign': 'center', 'margin': '48px 0', 'fontFamily': 'system-ui'}),
     dcc.Tabs(id="tabs", children=[
-        dcc.Tab(label='Tab one',
-                    children=[
-        html.H3(myheading1),
-        html.Div([
-            html.Img(src=app.get_asset_url(image1), style={'width': '30%', 'height': 'auto'}, className='four columns'),
+        dcc.Tab(label='Predicting Home Sale Prices in Ames, Iowa',
+        children=[
+            #html.H3(myheading1),
             html.Div([
-                html.H3('Features of Home:'),
-                html.Div(
-                    children=[
-                        html.Div('Year Built:', style={
-                            'display':'inline-block', 'padding': 5, 'flex': 1}),
-                        dcc.Input(id='YearBuilt', value=2010, type='number',
-                                  min=2006, max=2010, step=1, style={
-                                'display': 'inline-block', 'height': 26,}),
-                    ], style={'width': '49%', 'display': 'inline-block'}),
-                html.Br(),html.Br(),
-                html.Div(
-                    children=[
-                        html.Div('Bathrooms:', style={
-                            'display':'inline-block'}),
-                        # dcc.Input(id='Bathrooms', value=2, type='number',
-                        #           min=1, max=5, step=1, style={
-                        #         'display': 'inline-block', 'height': 26}),
-                        dcc.Slider(id='Bathrooms', min=1, max=5, 
-                                   marks={i: f' {i}' if i == 1 else str(i) for i in range(1, 6)}, value=2,),
-                        
-                        html.Div('Bedrooms:', style={'display':'inline-block'}),
-                        # dcc.Input(id='BedroomAbvGr1', value=4, type='number', min=1, max=5, step=1, style={
-                        #         'display': 'inline-block', 'height': 26}),
-                        dcc.Slider(id='BedroomAbvGr', min=1, max=5, 
-                                   marks={i: f'{i}' if i == 1 else str(i) for i in range(1, 6)}, value=2,),
-                    ], style={'width': '70%', 'display': 'inline-block'}),
-                #html.Div('Bedrooms:', style={'display':'inline-block'}),
-                html.Br(),html.Br(),
-                html.Div(
-                    children=[
-                        html.Div('Total Square Feet:', style={
-                            'display':'inline-block', 'padding': 5, 'flex': 1}),
-                        dcc.Input(id='TotalSF', value=2000, type='number',
-                                  min=100, max=5000, step=1, style={
-                                'display': 'inline-block', 'height': 26}),
-                    ], style={'width': '49%', 'display': 'inline-block'}),
-                html.Br(),html.Br(),
-                dcc.Checklist(id='SingleFam', options=[{'label': 'Single Family Home', 'value': 1}], value=[],),
-                dcc.Checklist(id='LargeNeighborhood', options=[{'label': 'Large Neighborhood', 'value': 1}], value=[],),
-                html.Br(),html.Br(),
-                html.Div(
-                    children=[
-                        html.Div('Car Garage:', style={
-                            'display':'inline-block'}),
-                        # dcc.Input(id='Bathrooms', value=2, type='number',
-                        #           min=1, max=5, step=1, style={
-                        #         'display': 'inline-block', 'height': 26}),
-                        dcc.Slider(id='GarageCars', min=0, max=4, 
-                                   marks={i: f' {i}' if i == 1 else str(i) for i in range(0, 5)}, value=0,),
-                        
-                        html.Div('When Remodeled (Years ago):', style={
-                            'display':'inline-block', 'padding': 5, 'flex': 1}),
-                        dcc.Input(id='RecentYearModAdd', value=0, type='number',
-                                  min=0, max=70, step=1, style={
-                                'display': 'inline-block', 'height': 26}),
-                    ], style={'width': '70%', 'display': 'inline-block'}),
-                # html.Div('Car Garage:'),
-                # dcc.Input(id='GarageCars', value=0, type='number', min=0, max=4, step=1),
-                # html.Div('Total Square Feet:'),
-                # dcc.Input(id='TotalSF', value=2000, type='number', min=100, max=5000, step=1),
-                # html.Div('When Remodeled (Years ago):'),
-                # dcc.Input(id='RecentYearModAdd', value=0, type='number', min=0, max=70, step=1),
-            ], className='four columns'),
-            html.Div([
-                html.Button(
-                    children='Submit', id='submit-val', n_clicks=0,
-                    style={
-                        'background-color': 'red',
-                        'color': 'white',
-                        'margin-left': '5px',
-                        'verticalAlign': 'center',
-                        'horizontalAlign': 'center'}
-                ),
-                html.H3('Predicted Home Value:'),
-                html.Div(id='Results')
-            ], className='four columns')
-        ], className='twelve columns',),
-        html.Br(),
-        html.Br(),
-        html.Br(),
-        html.H4('Regression Equation:'),
-        html.Div('Predicted Price = (- $662.7K Baseline) + ($0.35K * Year Built) + ($8.1K * Bathrooms) + (- $4.9K * Bedrooms) + ($0.042K * Total Square Feet) + ($ 22.1K * Single Family Home) + (- $7.9K * Large Neighborhood) + (19.5K * Car Garage) + (- $0.5K * When Remodeled (Years ago)'),
-        html.Br(),
-        html.A('Google Spreadsheet', href='https://docs.google.com/spreadsheets/d/1q2ustRvY-GcmPO5NYudvsBEGNs5Na5p_8LMeS4oM35U/edit?usp=sharing'),
-        html.Br(),
-        html.A('Code on Github', href=githublink),
-        html.Br(),
-        html.A("Data Source", href=sourceurl),
-    ]),
-        dcc.Tab(label='Tab two', children=[
+                html.Img(src=app.get_asset_url(image1), style={'width': '30%', 'height': 'auto'}, className='four columns'),
+                html.Div([
+                    html.H3('Features of Home:'),
+                    html.Div(
+                        children=[
+                            html.Div('Year Built:', style={
+                                'display':'inline-block', 'padding': 5, 'flex': 1}),
+                            dcc.Input(id='YearBuilt', value=2010, type='number',
+                                    min=2006, max=2010, step=1, style={
+                                    'display': 'inline-block', 'height': 26,}),
+                        ], style={'width': '49%', 'display': 'inline-block'}),
+                    html.Br(),html.Br(),
+                    html.Div(
+                        children=[
+                            html.Div('Bathrooms:', style={
+                                'display':'inline-block'}),
+                            # dcc.Input(id='Bathrooms', value=2, type='number',
+                            #           min=1, max=5, step=1, style={
+                            #         'display': 'inline-block', 'height': 26}),
+                            dcc.Slider(id='Bathrooms', min=1, max=5, 
+                                    marks={i: f' {i}' if i == 1 else str(i) for i in range(1, 6)}, value=2,),
+                            
+                            html.Div('Bedrooms:', style={'display':'inline-block'}),
+                            # dcc.Input(id='BedroomAbvGr1', value=4, type='number', min=1, max=5, step=1, style={
+                            #         'display': 'inline-block', 'height': 26}),
+                            dcc.Slider(id='BedroomAbvGr', min=1, max=5, 
+                                    marks={i: f'{i}' if i == 1 else str(i) for i in range(1, 6)}, value=2,),
+                        ], style={'width': '70%', 'display': 'inline-block'}),
+                    #html.Div('Bedrooms:', style={'display':'inline-block'}),
+                    html.Br(),html.Br(),
+                    html.Div(
+                        children=[
+                            html.Div('Total Square Feet:', style={
+                                'display':'inline-block', 'padding': 5, 'flex': 1}),
+                            dcc.Input(id='TotalSF', value=2000, type='number',
+                                    min=100, max=5000, step=1, style={
+                                    'display': 'inline-block', 'height': 26}),
+                        ], style={'width': '70%', 'display': 'inline-block'}),
+                    html.Br(),html.Br(),
+                    dcc.Checklist(id='SingleFam', options=[{'label': 'Single Family Home', 'value': 1}], value=[],),
+                    dcc.Checklist(id='LargeNeighborhood', options=[{'label': 'Large Neighborhood', 'value': 1}], value=[],),
+                    html.Br(),html.Br(),
+                    html.Div(
+                        children=[
+                            html.Div('Car Garage:', style={
+                                'display':'inline-block'}),
+                            # dcc.Input(id='Bathrooms', value=2, type='number',
+                            #           min=1, max=5, step=1, style={
+                            #         'display': 'inline-block', 'height': 26}),
+                            dcc.Slider(id='GarageCars', min=0, max=4, 
+                                    marks={i: f' {i}' if i == 1 else str(i) for i in range(0, 5)}, value=0,),
+                            
+                            html.Div('When Remodeled (Years ago):', style={
+                                'display':'inline-block', 'padding': 5, 'flex': 1}),
+                            dcc.Input(id='RecentYearModAdd', value=0, type='number',
+                                    min=0, max=70, step=1, style={
+                                    'display': 'inline-block', 'height': 26}),
+                        ], style={'width': '70%', 'display': 'inline-block'}),
+                    # html.Div('Car Garage:'),
+                    # dcc.Input(id='GarageCars', value=0, type='number', min=0, max=4, step=1),
+                    # html.Div('Total Square Feet:'),
+                    # dcc.Input(id='TotalSF', value=2000, type='number', min=100, max=5000, step=1),
+                    # html.Div('When Remodeled (Years ago):'),
+                    # dcc.Input(id='RecentYearModAdd', value=0, type='number', min=0, max=70, step=1),
+                ], className='four columns'),
+                html.Div([
+                    html.Button(
+                        children='Submit', id='submit-val', n_clicks=0,
+                        style={
+                            'background-color': 'red',
+                            'color': 'white',
+                            'margin-left': '5px',
+                            'verticalAlign': 'center',
+                            'horizontalAlign': 'center'}
+                    ),
+                    html.H3('Predicted Home Value:'),
+                    html.Div(id='Results')
+                ], className='four columns')
+            ], className='twelve columns',),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+            html.H4('Regression Equation:'),
+            html.Div('Predicted Price = (- $662.7K Baseline) + ($0.35K * Year Built) + ($8.1K * Bathrooms) + (- $4.9K * Bedrooms) + ($0.042K * Total Square Feet) + ($ 22.1K * Single Family Home) + (- $7.9K * Large Neighborhood) + (19.5K * Car Garage) + (- $0.5K * When Remodeled (Years ago)'),
+            html.Br(),
+            html.A('Google Spreadsheet', href='https://docs.google.com/spreadsheets/d/1q2ustRvY-GcmPO5NYudvsBEGNs5Na5p_8LMeS4oM35U/edit?usp=sharing'),
+            html.Br(),
+            html.A('Code on Github', href=githublink),
+            html.Br(),
+            html.A("Data Source", href=sourceurl),
+        ]),
+        dcc.Tab(label='Cancer prediction', children=[
             html.Div([
                 html.H1("This is the content in tab 2"),
                 html.P("A graph here would be nice!"),
@@ -131,25 +131,6 @@ app.layout = html.Div([
                              'type': 'barpolar', 'name': 'SF'},
                             {'x': [1, 2, 3], 'y': [6, 8, 1],
                              'type': 'barpolar', 'name': u'Montréal'},
-                        ],
-                        'layout': {
-                            'title': '2 Dash Data Visualization'
-                        }
-                    }
-                )
-            ])
-        ]),
-        dcc.Tab(label='Tab three', children=[
-            html.Div([
-                html.H1("This is the content in tab 3"),
-                dcc.Graph(
-                    id='example-graph2',
-                    figure={
-                        'data': [
-                            {'x': [1, 2, 3], 'y': [5, 2, 3],
-                             'type': 'scatter', 'name': 'SF'},
-                            {'x': [1, 2, 3], 'y': [6, 8, 1],
-                             'type': 'scatter', 'name': u'Montréal'},
                         ],
                         'layout': {
                             'title': '2 Dash Data Visualization'
